264857d7aa51d768971799ce00007c362587f7cc
emzhran <zachran750@gmail.com>
2025-01-20 23:30:59 +0700

    membuat DormitoryContainer untuk koneksi retrofit

---
+++ app/src/main/java/com/example/mohammadzachranzachary118/repository/DormitoryContainer.kt
@@ -0,0 +1,22 @@
+package com.example.mohammadzachranzachary118.repository
+
+
+import com.example.mohammadzachranzachary118.service.BangunanService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val bangunanRepository: BangunanRepository
+}
+class DormitoryContainer : AppContainer{
+    private val baseUrl = "http://10.0.2.2/pamTA/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val bangunanService: BangunanService by lazy { retrofit.create(BangunanService::class.java) }
+    override val bangunanRepository: BangunanRepository by lazy { NetworkBangunanRepository(bangunanService) }
+}
