cd789e073d557636b30632309da7f9463a91b8a7
emzhran <zachran750@gmail.com>
2025-01-21 03:48:28 +0700

    memanggil DormitoryApp di MainActivity

--- app/src/main/java/com/example/mohammadzachranzachary118/MainActivity.kt
+++ app/src/main/java/com/example/mohammadzachranzachary118/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mohammadzachranzachary118.ui.theme.Mohammadzachranzachary118Theme
+import com.example.mohammadzachranzachary118.ui.viewmodel.penyedia.DormitoryApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mohammadzachranzachary118Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DormitoryApp()
             }
         }
     }
