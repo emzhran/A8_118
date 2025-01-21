ad0aa33455baf50cdaa8a177c1cfd2533222b999
emzhran <zachran750@gmail.com>
2025-01-21 14:48:04 +0700

    penambahan KamarService dan KamarRepository pada DormitoryContainer

--- app/src/main/java/com/example/mohammadzachranzachary118/repository/DormitoryContainer.kt
+++ app/src/main/java/com/example/mohammadzachranzachary118/repository/DormitoryContainer.kt
@@ -2,6 +2,7 @@ package com.example.mohammadzachranzachary118.repository
 
 
 import com.example.mohammadzachranzachary118.service.BangunanService
+import com.example.mohammadzachranzachary118.service.KamarService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -9,6 +10,7 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val bangunanRepository: BangunanRepository
+    val kamarRepository: KamarRepository
 }
 class DormitoryContainer : AppContainer{
     private val baseUrl = "http://10.0.2.2/pamTA/"
@@ -18,5 +20,7 @@ class DormitoryContainer : AppContainer{
         .baseUrl(baseUrl).build()
 
     private val bangunanService: BangunanService by lazy { retrofit.create(BangunanService::class.java) }
+    private val kamarService: KamarService by lazy { retrofit.create(KamarService::class.java) }
     override val bangunanRepository: BangunanRepository by lazy { NetworkBangunanRepository(bangunanService) }
+    override val kamarRepository: KamarRepository by lazy { NetworkKamarRepository(kamarService) }
 }
