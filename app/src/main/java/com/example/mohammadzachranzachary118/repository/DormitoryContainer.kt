75e0e03ade8673dae7dd2c28798b7cce91b96a48
emzhran <zachran750@gmail.com>
2025-01-22 13:39:20 +0700

    penambahan MahasiswaRepository dan MahasiswaService pada DormitoryContainer

--- app/src/main/java/com/example/mohammadzachranzachary118/repository/DormitoryContainer.kt
+++ app/src/main/java/com/example/mohammadzachranzachary118/repository/DormitoryContainer.kt
@@ -3,6 +3,7 @@ package com.example.mohammadzachranzachary118.repository
 
 import com.example.mohammadzachranzachary118.service.BangunanService
 import com.example.mohammadzachranzachary118.service.KamarService
+import com.example.mohammadzachranzachary118.service.MahasiswaService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -11,6 +12,7 @@ import retrofit2.Retrofit
 interface AppContainer {
     val bangunanRepository: BangunanRepository
     val kamarRepository: KamarRepository
+    val mahasiswaRepository : MahasiswaRepository
 }
 class DormitoryContainer : AppContainer{
     private val baseUrl = "http://10.0.2.2/pamTA/"
@@ -21,6 +23,8 @@ class DormitoryContainer : AppContainer{
 
     private val bangunanService: BangunanService by lazy { retrofit.create(BangunanService::class.java) }
     private val kamarService: KamarService by lazy { retrofit.create(KamarService::class.java) }
+    private val mahasiswaService: MahasiswaService by lazy { retrofit.create(MahasiswaService::class.java) }
     override val bangunanRepository: BangunanRepository by lazy { NetworkBangunanRepository(bangunanService) }
     override val kamarRepository: KamarRepository by lazy { NetworkKamarRepository(kamarService) }
+    override val mahasiswaRepository : MahasiswaRepository by lazy { NetworkMahasiswaRepository(mahasiswaService) }
 }
