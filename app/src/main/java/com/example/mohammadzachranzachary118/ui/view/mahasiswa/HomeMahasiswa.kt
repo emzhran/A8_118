0c2aa7967713ffc11e96312d772381f01c8d35b3
emzhran <zachran750@gmail.com>
2025-01-22 19:26:46 +0700

    membuat halaman home mahasiswa

---
+++ app/src/main/java/com/example/mohammadzachranzachary118/ui/view/mahasiswa/HomeMahasiswa.kt
@@ -0,0 +1,262 @@
+package com.example.mohammadzachranzachary118.ui.view.mahasiswa
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mohammadzachranzachary118.R
+import com.example.mohammadzachranzachary118.model.Mahasiswa
+import com.example.mohammadzachranzachary118.ui.customwidget.TopAppBarr
+import com.example.mohammadzachranzachary118.ui.navigasi.DestinasiNavigasi
+import com.example.mohammadzachranzachary118.ui.viewmodel.mahasiswa.HomeMahasiswaState
+import com.example.mohammadzachranzachary118.ui.viewmodel.mahasiswa.HomeMahasiswaViewModel
+import com.example.mohammadzachranzachary118.ui.viewmodel.penyedia.PenyediaViewModel
+
+object DestinasiHomeMahasiswa: DestinasiNavigasi {
+    override val route = "home_mahasiswa"
+    override val titleRes = "Daftar Mahasiswa"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeMahasiswaScreen(
+    navigateBack: () -> Unit,
+    navigateToItemEntry: () -> Unit,
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier,
+    viewModel: HomeMahasiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(Unit) {
+        viewModel.getMhs()
+    }
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarr(
+                title = DestinasiHomeMahasiswa.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = navigateToItemEntry,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Tambah Mahasiswa"
+                    )
+                },
+                text = {
+                    Text(text = "Tambah Mahasiswa")
+                },
+                containerColor = colorResource(R.color.primary),
+                contentColor = Color.White,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    ) { innerPadding ->
+        HomeStatusMahasiswa(
+            homeMahasiswaState = viewModel.mahasiwaHomeState,
+            retryAction = { viewModel.getMhs() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = { mahasiswa ->
+                viewModel.deleteMhs(mahasiswa.idmahasiswa)
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatusMahasiswa(
+    homeMahasiswaState: HomeMahasiswaState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Mahasiswa) -> Unit
+) {
+    when (homeMahasiswaState) {
+        is HomeMahasiswaState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeMahasiswaState.Success -> {
+            if (homeMahasiswaState.mahasiswa.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Mahasiswa")
+                }
+            } else {
+                MahasiswaLayout(
+                    mahasiswa = homeMahasiswaState.mahasiswa,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = onDetailClick,
+                    onDeleteClick = onDeleteClick
+                )
+            }
+        }
+        is HomeMahasiswaState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.connection_loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.connection_error), contentDescription = null
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = retryAction,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.primary)
+            )
+        ) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun MahasiswaLayout(
+    mahasiswa: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Mahasiswa) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(mahasiswa) { mhs ->
+            MahasiswaCard(
+                mahasiswa = mhs,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(mhs.idmahasiswa) },
+                onDeleteClick = onDeleteClick
+            )
+        }
+    }
+}
+
+@Composable
+fun MahasiswaCard(
+    mahasiswa: Mahasiswa,
+    onDeleteClick: (Mahasiswa) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = mahasiswa.nama,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.AccountBox, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = mahasiswa.nomoridentitas,
+                    fontSize = 18.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Email, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = mahasiswa.email,
+                    fontSize = 18.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(onClick = { onDeleteClick(mahasiswa) }) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Hapus")
+                }
+            }
+        }
+    }
+}
