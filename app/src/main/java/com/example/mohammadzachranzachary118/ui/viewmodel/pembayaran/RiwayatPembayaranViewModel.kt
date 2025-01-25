481e211413c24f6e5599f849eff8470a01121b92
emzhran <zachran750@gmail.com>
2025-01-26 04:26:58 +0700

    membuat viewmodel untuk data riwayat pembayaran mahasiswa

---
+++ app/src/main/java/com/example/mohammadzachranzachary118/ui/viewmodel/pembayaran/RiwayatPembayaranViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.mohammadzachranzachary118.ui.viewmodel.pembayaran
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.mohammadzachranzachary118.model.Pembayaran
+import com.example.mohammadzachranzachary118.repository.PembayaranRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class RiwayatPembayaranState{
+    data class Success(val pembayaran: List<Pembayaran>): RiwayatPembayaranState()
+    object Error: RiwayatPembayaranState()
+    object Loading: RiwayatPembayaranState()
+}
+
+class RiwayatPembayaranViewModel(private val pmbyrn: PembayaranRepository) : ViewModel() {
+    var riwayatPembayaranState: RiwayatPembayaranState by mutableStateOf(RiwayatPembayaranState.Loading)
+        private set
+
+
+    fun getRiwayat(id_mahasiswa: String) {
+        viewModelScope.launch {
+            riwayatPembayaranState = RiwayatPembayaranState.Loading
+            riwayatPembayaranState = try {
+                RiwayatPembayaranState.Success(pmbyrn.getPembayaran().filter { it.idmahasiswa == id_mahasiswa })
+            } catch (e: IOException) {
+                RiwayatPembayaranState.Error
+            } catch (e: HttpException) {
+                RiwayatPembayaranState.Error
+            }
+        }
+    }
+
+    fun deletePembayaran(id_pembayaran: String) {
+        viewModelScope.launch {
+            try {
+                pmbyrn.deletePembayaran(id_pembayaran)
+            } catch (e: IOException) {
+                RiwayatPembayaranState.Error
+            } catch (e: HttpException) {
+                RiwayatPembayaranState.Error
+            }
+        }
+    }
+}
